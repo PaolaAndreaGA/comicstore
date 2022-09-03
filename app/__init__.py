1b8590f7e7726a03da2de45c43c5de196594662f
PaolaAndreaGA <paga0209@gmial.com>
2022-09-03 00:34:40 -0500

    [ADD] Templates & bootstrap connection

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hola mundo"
+    return render_template('index.html')
+
 
 def pag_no_encontrada(error):
-  return render_template('errores/404.html'), 404
+    return render_template('errores/404.html'), 404
 
 
 def inicializar_app():
-  app.register_error_handler(404, pag_no_encontrada)
-  return app
+    app.register_error_handler(404, pag_no_encontrada)
+    return app
