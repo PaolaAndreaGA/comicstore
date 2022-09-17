98667765d6d39dc02622874553c3df6927994478
PaolaAndreaGA <paga0209@gmial.com>
2022-09-16 22:21:22 -0500

    [ADD]cx to db

--- config.py
+++ config.py
@@ -2,11 +2,15 @@ from decouple import config
 
 
 class Config:
-    SECRET_KEY='Andrea8727.'
+    SECRET_KEY = 'Andrea8727.'
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'comicsworldstore'
 
 
 config = {
